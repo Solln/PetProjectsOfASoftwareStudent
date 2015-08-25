3cd32e86a0e2a2f5624523da43912ed128dd36a1
Solln <zingy@pheonix.local>
2015-08-25 16:15:24 +0100

    Count Words In A String

---
+++ CountWordsInAString/src/com/company/Test/MainTest.java
@@ -0,0 +1,20 @@
+package com.company.Test;
+
+
+public class MainTest {
+
+    @org.junit.Test
+    public void testMain() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetString() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCountWords() throws Exception {
+
+    }
+}
