e97bf58f5c8c65dd82110cf052e7d29a8f10c1be
Solln <zingy@pheonix.local>
2015-08-25 11:23:35 +0100

    Added Tests

--- Palindrome/src/com/company/Test/MainTest.java
+++ Palindrome/src/com/company/Test/MainTest.java
@@ -1,13 +1,26 @@
 package com.company.Test;
 
+import com.company.Main.Main;
+import junit.framework.Assert;
+
 public class MainTest {
 
+    Main main = new Main();
+
+
     @org.junit.Test
-    public void testMain() throws Exception {
+    public void testPalindromeCheckerTrue() throws Exception {
+
+        Assert.assertTrue(main.isPalindrome(("hannah").toCharArray()));
 
     }
 
+    @org.junit.Test
+    public void testPalindromeCheckerFalse() throws Exception {
 
+        Assert.assertFalse(main.isPalindrome(("hnnah").toCharArray()));
+
+    }
 
 
 
